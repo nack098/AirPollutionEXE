0c13994cdf1f6f0c7f29f7405ea08e5cfb764af0
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-01-18 19:37:56 +0700

    changed output format for fetching

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import LatitudeInput from "./components/LatitudeInput";
 import LongitudeInput from "./components/LongitudeInput";
 import { useSelector } from "react-redux";
 import { RootState } from "./states/store";
+import { useState } from "react";
 
 function App() {
   const date = useSelector((state: RootState) => state.date.value);
   const lat = useSelector((state: RootState) => state.lat.value);
   const long = useSelector((state: RootState) => state.long.value);
-  const pm25 = 0.0;
+  const [pm25, setPm25] = useState("");
   return (
     <div>
       <h1>Thailand PM 2.5 Predictor 2025</h1>
@@ -21,6 +22,7 @@ function App() {
       <LongitudeInput />
       <h3>Selected latitude: {lat}</h3>
       <h3>Selected longitude: {long}</h3>
+      <button onClick={() => setPm25(`${lat}+${long}+${date}`)}>Predict</button>
       <h3>PM 2.5 level: {pm25}</h3> {/*placeholder for PM 2.5 level*/}
     </div>
   );

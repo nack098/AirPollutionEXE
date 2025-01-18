943c6a1b512ab80943a14b10952f80dbc9664963
Summation <wachirawichkanil@gmail.com>
2025-01-18 21:07:11 +0700

    did not actually implement cors
    nack help

--- flask_backend/server.py
+++ flask_backend/server.py
@@ -1,15 +1,12 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
 import datetime
 import random
 
 # Initializing flask app
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/api/predict', methods=['GET','POST'])
-@cross_origin()
 def predict_pm25():
     if request.method == 'POST':
         print(request.body)
